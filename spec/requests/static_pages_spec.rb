b3198e9ae772eff1449b97f6a1f7378f9d2b0ad1
Phil <p.railstesting@gmail.com>
2012-01-29 20:12:51 +0000

    Updated styles & Moved default Rails page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,37 +3,37 @@ require 'spec_helper'
 describe "Static pages" do
   describe "Home page" do
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('h1', :text => 'Sample App')
     end
     it "should have the correct title" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('title', :text => ' | Home')
     end
   end
 
   describe "Help page" do
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('h1', :text => 'Help')
     end
   end
 
   describe "About page" do
     it "should have the content 'About'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('h1', :text => 'About')
     end
   end
 
   describe "Contact page" do
     it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_selector('h1', :text => 'Contact')
     end
 
     it "should have the correct title" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_selector('title', :text => '| Contact')
     end
   end
